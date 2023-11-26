63305eb914faf094595106fce32a0d636f9f81de
Prajwal Singhania <prajwal1210@gmail.com>
2023-11-26 00:33:33 -0500

    Added support for topk attention for Llama models

--- top_k/utils.py
+++ top_k/utils.py
@@ -14,3 +14,18 @@ def mask_top_k_elements_3d(tensor, k, dim=2):
     # masked_tensor = tensor * mask
 
     return mask
+
+
+def test_mask():
+    test_tensor = torch.rand(1, 1, 5, 5)
+    print (test_tensor)
+    ones = torch.ones_like(test_tensor, dtype=torch.bool)
+    tril_mask = torch.tril(ones, diagonal=0)
+    test_tensor[~tril_mask] = float('-inf')
+    print (test_tensor)
+
+    test_tensor = mask_top_k_elements_3d(test_tensor, 2, dim=3)
+    print (test_tensor)
+
+if __name__ == "__main__":
+    test_mask()
