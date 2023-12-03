447e079d093e08fc31627f8d353c46b9bddc1038
siddhant-bharti <sbharti51@gmail.com>
2023-12-02 20:22:08 -0500

    Adds GpuIndexFlatIP support for faiss attention

--- top_k/utils.py
+++ top_k/utils.py
@@ -1,4 +1,39 @@
+import faiss
+import faiss.contrib.torch_utils
 import torch
+
+# Supported Types of indexes:
+# GpuIndexFlatIP
+INDEX_TYPE = 'GpuIndexFlatIP'
+
+
+def get_index(index_type, vector_size):
+    index = None
+    if index_type == 'GpuIndexFlatIP':
+        index = faiss.GpuIndexFlatIP(faiss.StandardGpuResources(), vector_size)
+    return index
+
+
+def faiss_attention(query_states, key_states, k):
+    """
+    ToDo(Siddhant): This method assumes shape of these tensors as num_heads, seq_len, hidden_size which is not the case
+    for all LMs. Fix this.
+    """
+    num_heads, seq_len, hidden_size = query_states.shape
+    topk = min(k, seq_len)
+    indexes = [get_index(INDEX_TYPE, hidden_size) for _ in range(num_heads)]
+    attention = torch.full((num_heads, seq_len, hidden_size), float('-inf')).cuda(query_states.device)
+
+    for head_index, index in enumerate(indexes):
+        index.add(key_states[head_index])
+        attn_scores, attn_indexes = index.search(query_states[head_index], topk)
+        curr_attention = torch.full((seq_len, hidden_size), float('-inf')).cuda(query_states.device)
+        curr_attention.scatter_(-1, attn_indexes, attn_scores)
+        attention[head_index] = curr_attention
+    # todo(Siddhant): how to delete the entire index?
+    for _index in indexes:
+        _index.reset()
+    return attention
 
 
 def mask_top_k_elements_3d(tensor, k, dim=2):
