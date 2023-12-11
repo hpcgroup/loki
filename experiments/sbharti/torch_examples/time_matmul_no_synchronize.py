aab61426f0574aa7f749f3af28aea5ac486c2acc
siddhant-bharti <sbharti51@gmail.com>
2023-12-11 16:09:45 -0500

    Adds time_matmul_no_sync

---
+++ experiments/sbharti/torch_examples/time_matmul_no_synchronize.py
@@ -0,0 +1,20 @@
+import time
+
+import torch
+
+DIM = 500
+NUM_SAMPLES = 1
+
+times = []
+for e in range(NUM_SAMPLES):
+    mat1 = torch.rand(DIM, DIM).cuda()
+    mat2 = torch.rand(DIM, DIM).cuda()
+    start_epoch = time.perf_counter()
+    torch.mm(mat1, mat2)
+    end_epoch = time.perf_counter()
+    elapsed = end_epoch - start_epoch
+    times.append(elapsed)
+    print(f"Executed matmul on 2 {DIM}x{DIM} matrix in {elapsed:.4f} seconds")
+
+avg_time = sum(times)/NUM_SAMPLES
+print("Average time taken: ", avg_time)
