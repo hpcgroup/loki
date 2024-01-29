15071fa1c19329acdf2fb555a59ca542a9bf0d6f
Prajwal Singhanaia <prajwal1210@gmail.com>
2024-01-29 12:57:50 -0500

    Text generation initial commit

---
+++ text-generation/profile_text_generation.py
@@ -0,0 +1,7 @@
+from datasets import load_dataset
+
+# Load the dataset 
+# We are using a dataset from the HuggingFace Hub
+dataset = load_dataset("MohamedRashad/ChatGPT-prompts")
+
+print (dataset)
