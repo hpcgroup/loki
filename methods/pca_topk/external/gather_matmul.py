10f16ac7685ce0acb1774fda8a4ec2a3c85294c4
Prajwal Singhania <prajwal1210@gmail.com>
2024-05-06 15:06:43 -0700

    Added compute optimised modify_llama_optimised in pca_topk

---
+++ methods/pca_topk/external/gather_matmul.py
@@ -0,0 +1,234 @@
+# Copyright (c) 2024 Graphcore Ltd. All rights reserved.
+# This code was copied from https://github.com/graphcore-research/llm-inference-research/blob/benchmarks/src/gather_matmul.py
+
+# MIT License
+# 
+# Copyright (c) 2024 Graphcore Ltd.
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import math
+import warnings
+
+import torch
+import triton
+import triton.language as tl
+from torch import Tensor
+
+
+@triton.jit
+def _kernel_gather_inner_bmv(
+    A_ptr,
+    B_ptr,
+    I_ptr,
+    Y_ptr,
+    k: tl.constexpr,  # int
+    n: int,
+    n_chunk: tl.constexpr,  # int
+    A_s0: int,
+    A_s2: int,
+    B_s0: int,
+    B_s1: int,
+    B_s2: int,
+    I_s0: int,
+    I_s1: int,
+    gather_A: tl.constexpr,  # bool
+):
+    pid = tl.program_id(axis=0).to(tl.int64)
+    i = tl.load(I_ptr + pid * I_s0 + tl.arange(0, k) * I_s1)  # (k)
+    a = tl.load(A_ptr + pid * A_s0 + (i if gather_A else tl.arange(0, k)) * A_s2)  # (k)
+    for chunk in range(0, tl.cdiv(n, n_chunk)):
+        chunk_idx = chunk * n_chunk + tl.arange(0, n_chunk)
+        b = tl.load(  # (k x n_chunk)
+            B_ptr + pid * B_s0 + (i * B_s1)[:, None] + (chunk_idx * B_s2)[None, :]
+        )
+        # As tl.dot() is unavailable for matrix-vector
+        y = tl.sum(a[:, None] * b, 0)  # (n_chunk)
+        tl.store(Y_ptr + pid * n + chunk_idx, y, mask=(chunk_idx < n))
+
+
+def gather_inner_bmv(
+    A: Tensor, B: Tensor, I: Tensor, chunk: int, _matrix_only: bool = False
+) -> Tensor:
+    """Batched vector-matrix multiplication, with a gather on the inner dimension.
+
+    Dimensions:
+       b -- batch
+       k* -- (pre-gather) inner dimension
+       k -- (post-gather) inner dimension (k <= k*), must be a power of two
+       n -- outer dimension
+
+    A -- (b, 1, k*)        batch of vectors
+    B -- (b, k*, n)        batch of matrices
+    I -- int(b, k)         indices, in [0, k*)
+    chunk -- int           size of chunks of `B` (along dimension `n`) to be processed at a time
+    _matrix_only -- bool   don't use (see `gather_inner_matrix_only_bmv`)
+
+    returns -- (b, 1, n)   the inner product of `A` and `B`, after gathering the inner dimension
+                           according to `I`
+    """
+    if A.ndim > 3:
+        assert B.ndim == A.ndim and I.ndim == A.ndim - 1
+        return gather_inner_bmv(
+            A.flatten(end_dim=-3),
+            B.flatten(end_dim=-3),
+            I.flatten(end_dim=-2),
+            chunk=chunk,
+            _matrix_only=_matrix_only,
+        ).unflatten(0, A.shape[:-2])
+    assert A.ndim == 3 and B.ndim == 3 and A.shape[1] == 1
+    assert (
+        I.ndim == 2
+        and I.shape[0] == A.shape[0]
+        and 2 ** int(math.log2(I.shape[1])) == I.shape[1]
+    )
+    assert A.shape[2] == (I.shape[1] if _matrix_only else B.shape[1])
+    if B.stride(2) != 1:
+        warnings.warn(
+            "gather_inner_bmv(A, B, ...) `B` should be contiguous in the last dimension"
+            ", otherwise it is very slow"
+        )
+
+    b, k, n = A.shape[0], I.shape[1], B.shape[2]
+    Y = torch.empty((b, 1, n), dtype=A.dtype, device=A.device)
+    assert Y.stride(0) == n and Y.stride(2) == 1
+
+    _kernel_gather_inner_bmv[(b,)](
+        A_ptr=A,
+        B_ptr=B,
+        I_ptr=I,
+        Y_ptr=Y,
+        k=k,
+        n=n,
+        n_chunk=chunk,
+        A_s0=A.stride(0),
+        A_s2=A.stride(2),
+        B_s0=B.stride(0),
+        B_s1=B.stride(1),
+        B_s2=B.stride(2),
+        I_s0=I.stride(0),
+        I_s1=I.stride(1),
+        gather_A=not _matrix_only,
+    )
+    return Y
+
+
+def gather_inner_matrix_only_bmv(A: Tensor, B: Tensor, I: Tensor, chunk: int) -> Tensor:
+    """Batched vector-matrix multiplication, with a gather on the inner dimension of the matrix.
+
+    Dimensions:
+       b -- batch
+       k* -- (pre-gather) inner dimension
+       k -- (post-gather) inner dimension (k <= k*), must be a power of two
+       n -- outer dimension
+
+    A -- (b, 1, k)         batch of vectors
+    B -- (b, k*, n)        batch of matrices
+    I -- int(b, k)         indices, in [0, k*)
+    chunk -- int           size of chunks of `B` (along dimension `n`) to be processed at a time
+
+    returns -- (b, 1, n)   the inner product of `A` and `B`, after gathering the inner dimension
+                           of `B` according to `I`
+    """
+    return gather_inner_bmv(A, B, I, chunk=chunk, _matrix_only=True)
+
+
+@triton.jit
+def _kernel_gather_outer_bmv(
+    A_ptr,
+    B_ptr,
+    I_ptr,
+    Y_ptr,
+    k: tl.constexpr,
+    n: int,
+    n_chunk: tl.constexpr,
+    A_s0: int,
+    A_s2: int,
+    B_s0: int,
+    B_s1: int,
+    B_s2: int,
+    I_s0: int,
+    I_s1: int,
+):
+    pid = tl.program_id(axis=0).to(tl.int64)
+    a = tl.load(A_ptr + pid * A_s0 + tl.arange(0, k) * A_s2)  # (k)
+    for chunk in range(0, tl.cdiv(n, n_chunk)):
+        chunk_idx = chunk * n_chunk + tl.arange(0, n_chunk)
+        i = tl.load(I_ptr + pid * I_s0 + chunk_idx * I_s1)  # (n_chunk)
+        b = tl.load(  # (k x n_chunk)
+            B_ptr
+            + pid * B_s0
+            + (tl.arange(0, k) * B_s1)[:, None]
+            + (i * B_s2)[None, :],
+            mask=(chunk_idx < n)[None, :],
+        )
+        # # As tl.dot() is unavailable for matrix-vector
+        y = tl.sum(a[:, None] * b, 0)  # (n_chunk)
+        tl.store(Y_ptr + pid * n + chunk_idx, y, mask=(chunk_idx < n))
+
+
+def gather_outer_bmv(A: Tensor, B: Tensor, I: Tensor, chunk: int) -> Tensor:
+    """Batched vector-matrix multiplication, with a gather on the matrix outer dimension.
+
+    Dimensions:
+       b -- batch
+       k -- inner dimension, must be a power of two
+       n* -- (pre-gather) outer dimension
+       n -- (post-gather) outer dimension (n <= n*)
+
+    A -- (b, 1, k)         batch of vectors
+    B -- (b, k, n*)        batch of matrices
+    I -- int(b, n)         indices, in [0, n*)
+    chunk -- int           size of chunks of `B` (along dimension `n`) to be processed at a time
+
+    returns -- (b, 1, n)   the inner product of `A` and `B`, after gathering the outer dimension
+                           according to `I`
+    """
+    if A.ndim > 3:
+        assert B.ndim == A.ndim and I.ndim == A.ndim - 1
+        return gather_outer_bmv(
+            A.flatten(end_dim=-3),
+            B.flatten(end_dim=-3),
+            I.flatten(end_dim=-2),
+            chunk=chunk,
+        ).unflatten(0, A.shape[:-2])
+    assert A.ndim == 3 and B.ndim == 3 and A.shape[1] == 1 and A.shape[2] == B.shape[1]
+    assert I.ndim == 2 and I.shape[0] == A.shape[0]
+
+    b, k, n = A.shape[0], A.shape[2], I.shape[1]
+    Y = torch.empty((b, 1, n), dtype=A.dtype, device=A.device)
+    assert Y.stride(0) == n and Y.stride(2) == 1
+
+    _kernel_gather_outer_bmv[(b,)](
+        A_ptr=A,
+        B_ptr=B,
+        I_ptr=I,
+        Y_ptr=Y,
+        k=k,
+        n=n,
+        n_chunk=chunk,
+        A_s0=A.stride(0),
+        A_s2=A.stride(2),
+        B_s0=B.stride(0),
+        B_s1=B.stride(1),
+        B_s2=B.stride(2),
+        I_s0=I.stride(0),
+        I_s1=I.stride(1),
+    )
+    return Y
---
+++ methods/pca_topk/modify_llama_optimized.py
@@ -0,0 +1,167 @@
+from typing import List, Optional, Tuple, Union
+import math
+import warnings
+from transformers.models.llama.modeling_llama import LlamaAttention, repeat_kv, apply_rotary_pos_emb
+from transformers.models.llama.configuration_llama import LlamaConfig
+from transformers.models.llama.modeling_llama import LlamaAttention, LlamaMLP, ACT2FN
+from transformers.cache_utils import Cache
+import torch
+from torch import nn
+import torch.nn.functional as F
+from functools import partial
+
+from .utils import mask_attn_pca_topk, get_pca_components
+import methods.pca_topk.external.gather_matmul as G
+import methods
+
+
+try:
+    from axonn import axonn as ax
+    from axonn.intra_layer import drop
+    AXONN_AVAILABLE=True
+except ImportError:
+    AXONN_AVAILABLE=False
+
+def get_pca_forward(args):
+    def modified_forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_value: Optional[Cache] = None,
+        output_attentions: bool = False,
+        use_cache: bool = False,
+        **kwargs,
+    ) -> Tuple[torch.Tensor, Optional[torch.Tensor], Optional[Tuple[torch.Tensor]]]:
+        if "padding_mask" in kwargs:
+            warnings.warn(
+                "Passing `padding_mask` is deprecated and will be removed in v4.37. Please make sure use `attention_mask` instead.`"
+            )
+
+        bsz, q_len, _ = hidden_states.size()
+
+        if self.config.pretraining_tp > 1:
+            key_value_slicing = (self.num_key_value_heads * self.head_dim) // self.config.pretraining_tp
+            query_slices = self.q_proj.weight.split(
+                (self.num_heads * self.head_dim) // self.config.pretraining_tp, dim=0
+            )
+            key_slices = self.k_proj.weight.split(key_value_slicing, dim=0)
+            value_slices = self.v_proj.weight.split(key_value_slicing, dim=0)
+
+            query_states = [F.linear(hidden_states, query_slices[i]) for i in range(self.config.pretraining_tp)]
+            query_states = torch.cat(query_states, dim=-1)
+
+            key_states = [F.linear(hidden_states, key_slices[i]) for i in range(self.config.pretraining_tp)]
+            key_states = torch.cat(key_states, dim=-1)
+
+            value_states = [F.linear(hidden_states, value_slices[i]) for i in range(self.config.pretraining_tp)]
+            value_states = torch.cat(value_states, dim=-1)
+
+        else:
+            query_states = self.q_proj(hidden_states)
+            key_states = self.k_proj(hidden_states)
+            value_states = self.v_proj(hidden_states)
+
+        query_states = query_states.view(bsz, q_len, self.num_heads, self.head_dim).transpose(1, 2)
+        key_states = key_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        value_states = value_states.view(bsz, q_len, self.num_key_value_heads, self.head_dim).transpose(1, 2)
+        
+        past_key_value = getattr(self, "past_key_value", past_key_value)
+        cos, sin = self.rotary_emb(value_states, position_ids)
+        query_states, key_states = apply_rotary_pos_emb(query_states, key_states, cos, sin)
+
+        if not hasattr(self, "pca_components"):
+            _, self.pca_components, _= get_pca_components(args, self.layer_idx, self.head_dim, args.top_r, self.num_key_value_groups, repeat_kv)
+            self.pca_components = self.pca_components.to(query_states.dtype)
+
+            # TODO: Keep it fixed or make it dynamic?
+            if args.top_k <= 1:
+                args.top_k = int(args.top_k * key_states.shape[-2])
+            else:
+                args.top_k = int(args.top_k)
+
+        key_states = torch.matmul(key_states, self.pca_components)
+        query_states = torch.matmul(query_states, self.pca_components)
+
+        if past_key_value is not None:
+            cache_kwargs = {"sin": sin, "cos": cos}  # Specific to RoPE models
+            key_states, value_states = past_key_value.update(key_states, value_states, self.layer_idx, cache_kwargs)
+
+        key_states = repeat_kv(key_states, self.num_key_value_groups)
+        value_states = repeat_kv(value_states, self.num_key_value_groups)
+
+        # Generation Step
+        if query_states.shape[-2] == 1:
+            # Compute Approximate Attention Weights
+            # We do not need a causal mask here since this is the generation step
+            attn_weights = torch.matmul(query_states[:,:,:,:args.top_r], key_states.transpose(2, 3)[:,:,:16,:]) / math.sqrt(self.head_dim)
+
+            key_states_topk_indices = torch.topk(attn_weights, args.topk, dim=-1).indices.to("cuda")
+            key_states_topk_indices , _ = torch.sort(key_states_topk_indices, dim=-1)
+            key_states_topk_indices = key_states_topk_indices.reshape(-1, key_states_topk_indices.shape[-1])
+
+            key_states = key_states.reshape(-1, key_states.shape[-2], key_states.shape[-1])
+            query_states = query_states.reshape(-1, query_states.shape[-2], query_states.shape[-1])
+
+            attn_weights = G.gather_outer_bmv(
+                query_states,
+                key_states.transpose(-1, -2),
+                key_states_topk_indices,
+                chunk=256 # Varying this changes performance
+                #chunk=min(k2, 65536 // Q.shape[-1]),
+            )
+            attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+            attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
+
+            value_states = value_states.reshape(-1, value_states.shape[-2], value_states.shape[-1])
+            attn_output = G.gather_inner_matrix_only_bmv(
+              attn_weights, 
+              value_states, 
+              key_states_topk_indices, 
+              chunk=64
+            )
+            attn_output = attn_output.reshape(bsz, self.num_heads, q_len, self.head_dim)
+        else:
+            # Compute Standard Attention
+            attn_weights = (torch.matmul(query_states, key_states.transpose(2, 3))) / math.sqrt(self.head_dim)
+        
+            if attention_mask is not None:  # no matter the length, we just slice it
+                causal_mask = attention_mask[:, :, :, : key_states.shape[-2]]
+                attn_weights = attn_weights + causal_mask
+
+            # upcast attention to fp32
+            attn_weights = nn.functional.softmax(attn_weights, dim=-1, dtype=torch.float32).to(query_states.dtype)
+            attn_weights = nn.functional.dropout(attn_weights, p=self.attention_dropout, training=self.training)
+            attn_output = torch.matmul(attn_weights, value_states)
+
+        if attn_output.size() != (bsz, self.num_heads, q_len, self.head_dim):
+            raise ValueError(
+                f"`attn_output` should be of size {(bsz, self.num_heads, q_len, self.head_dim)}, but is"
+                f" {attn_output.size()}"
+            )
+
+        attn_output = attn_output.transpose(1, 2).contiguous()
+
+        attn_output = attn_output.reshape(bsz, q_len, self.hidden_size)
+
+        if self.config.pretraining_tp > 1:
+            attn_output = attn_output.split(self.hidden_size // self.config.pretraining_tp, dim=2)
+            o_proj_slices = self.o_proj.weight.split(self.hidden_size // self.config.pretraining_tp, dim=1)
+            attn_output = sum([F.linear(attn_output[i], o_proj_slices[i]) for i in range(self.config.pretraining_tp)])
+        else:
+            attn_output = self.o_proj(attn_output)
+
+        if not output_attentions:
+            attn_weights = None
+
+        return attn_output, attn_weights, past_key_value
+    return modified_forward
+
+def make_llama_attention_pca_topk(args):
+    print ("Modifying Llama Attention -> PCA TopK Attention")
+    print ("Top R:", args.top_r)
+    print ("Top K:", args.top_k)
+    #if args.optimised:
+    print ("Optimised PCA TopK Attention")
+    #LlamaAttention.__init__ = get_pca_init(top_r, top_k)
+    LlamaAttention.forward = get_pca_forward(args)
