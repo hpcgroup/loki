cbe06c81714d41795f311240eb0e1630cf8b33b4
Prajwal Singhania <prajwal1210@gmail.com>
2024-09-23 11:58:29 -0700

    Cleanup: Added common generation script for experiments [Partially Tested]

--- examples/h2o/h2o-mistral.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-sbatch examples/h2o/submit_h2o_1gpu.sh mistralai/Mistral-7B-v0.1 mistral 4096 0.125
-sbatch examples/h2o/submit_h2o_1gpu.sh mistralai/Mistral-7B-v0.1 mistral 4096 0.25
-sbatch examples/h2o/submit_h2o_1gpu.sh mistralai/Mistral-7B-v0.1 mistral 4096 0.125 --lm-harness-eval
-sbatch examples/h2o/submit_h2o_1gpu.sh mistralai/Mistral-7B-v0.1 mistral 4096 0.25 --lm-harness-eval
-set +x
