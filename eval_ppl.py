a36ee2d38194ecfc6304a27dddd8a9d8505fa024
Prajwal Singhania <prajwal1210@gmail.com>
2024-09-23 12:00:39 -0700

    Cleanup: Removed sparq, spark and pca initial implementations

--- eval_ppl.py
+++ eval_ppl.py
@@ -1,5 +1,5 @@
 from methods import init_tensor_saver
-from methods.common.configure_model import get_h2o_args, get_topk_args, get_spar_args, get_pca_args, get_save_tensor_args
+from methods.common.configure_model import get_h2o_args, get_topk_args, get_pca_args, get_save_tensor_args
 from methods.common.configure_model import get_modifier
 from methods import init_logger, finish_logger
 import methods
@@ -7,9 +7,11 @@ import methods
 import argparse
 import os
 
+# Required to avoid tokenizers warning
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-#LM_HARNESS_VALID_TASKS = ["hellaswag", "winogrande", "gsm8k", "mmlu", "truthfulqa_mc2", "arc_challenge"]
+# Hugging Face OpenLLM Tasks and associated metrics from May 2024
+# https://huggingface.co/spaces/open-llm-leaderboard-old/open_llm_leaderboard
 LM_HARNESS_TASKS = {
   "mmlu" : "acc,none",
   "gsm8k" : "exact_match,strict-match",
@@ -28,11 +30,10 @@ if __name__ == "__main__":
     parser.add_argument("--lm-harness-eval", action='store_true', default=False, help="use lm harness eval")
     parser.add_argument("--dataset", type=str, default="wikitext-test", help="dataset - wikitext, bookcorpus, c4")
     parser.add_argument("--use-wandb", action='store_true', default=False, help="use wandb")
-    #parser.add_argument("--task", type=str, default="perplexity", help="task - perplexity, <lm_harness_tasks>")
 
+    # Get Method Specific Arguments
     parser = get_h2o_args(parser)
     parser = get_topk_args(parser)
-    parser = get_spar_args(parser)
     parser = get_pca_args(parser)
     parser = get_save_tensor_args(parser)
     args = parser.parse_args()
@@ -44,21 +45,10 @@ if __name__ == "__main__":
 
     modifier_method = get_modifier(args)
     if modifier_method is None:
-        print ("Running Base HF Model without any modification")
-        #raise ValueError("Modifier method not found")
-
-    print (modifier_method)
-
-    cache = None
-    if args.use_topk or args.use_h2o or args.use_pca_topk:
+        print ("[INFO] Running Base HF Model without any modification")
+    else:
+        print (modifier_method)
         modifier_method(args)
-    elif args.use_sparq or args.use_spark:
-        modifier_method(args.top_r, args.top_k)
-    elif args.use_spar_hat:
-        cache = modifier_method(args.top_r)
-    elif args.use_pca:
-        modifier_method(args.top_r)
-        args.use_axonn = False
     
     if args.lm_harness_eval:
         import lm_eval
@@ -66,8 +56,10 @@ if __name__ == "__main__":
         use_axonn_low_level_api = True
 
         if args.model_type == "gptneox":
+            print ("[INFO] Disabling Axonn for GPT-NeoX")
             args.use_axonn = False
         if args.model_id == "mistralai/Mixtral-8x22B-v0.1" or args.use_h2o:
+            print ("[INFO] Disabling Low Level Axonn API for Mixtral-8x22B")
             use_axonn_low_level_api = False
         
         if args.use_h2o:
@@ -75,11 +67,12 @@ if __name__ == "__main__":
             del LM_HARNESS_TASKS["gsm8k"]
 
         if args.use_axonn:
+            # TODO: This is a hack to get the model from the evaluate function. Ideally, this should be refactored to a separate function
             model = evaluate(model_id=args.model_id,
                         dataset=args.dataset,
                         sequence_length=args.sequence_length,
                         use_axonn=args.use_axonn,
-                        past_key_values=cache,
+                        past_key_values=None,
                         axonn_low_level_api=use_axonn_low_level_api,
                         return_model=True)
             results = lm_eval.simple_evaluate(
@@ -104,7 +97,6 @@ if __name__ == "__main__":
                 methods.LOGGER.log_lm_harness_results(LM_HARNESS_TASKS, results["results"])
     else:
         from lm_perplexity_eval import evaluate
-        print(args.use_axonn)
         # Some issue with loading pythia with axonn
         use_axonn_low_level_api = True
         if args.model_type == "gptneox":
@@ -116,7 +108,6 @@ if __name__ == "__main__":
                     dataset=args.dataset,
                     sequence_length=args.sequence_length,
                     use_axonn=args.use_axonn,
-                    past_key_values=cache,
                     axonn_low_level_api=use_axonn_low_level_api)
 
         print(ppl)
--- methods/baselines/h2o_hf_opt/h2o_utils.py
+++ methods/baselines/h2o_hf_opt/h2o_utils.py
@@ -1,17 +1,8 @@
 # The function has been MODIFIED from the H2O github repo which is under the MIT Liscense
 # The original file can be found at https://github.com/FMInference/H2O/blob/main/h2o_hf/utils_lm_eval/modify_llama.py 
 
-'''
-Copyright <YEAR> <COPYRIGHT HOLDER>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-'''
-
+# The original H2O monkey patching code used a lot of memory and was slow for large models. 
+# This is a more efficient version of the code that is used for larger models
 import os
 import pdb
 import copy
@@ -23,9 +14,7 @@ from torch import nn
 import time
 
 def local_heavy_hitter_mask(attn_weights, heavy_budget):
-
     # attn_weights (BS, head, query, keys)
-
     dtype_attn_weights = attn_weights.dtype
     seq_length = attn_weights.shape[-1]
 
@@ -34,10 +23,8 @@ def local_heavy_hitter_mask(attn_weights, heavy_budget):
     accumulated_attention_score = torch.sum(tmp_attn[:,:,:heavy_budget,:], dim=-2) #(head, keys)
     
     mask_bottom = torch.zeros_like(attn_weights, dtype=torch.bool)
-    #alpha = torch.sum(mask_bottom, dim=-1, keepdim=True).to(dtype_attn_weights)
 
     mask_bottom[:,:, :heavy_budget, :heavy_budget] = True
-    #alpha[:, :, :heavy_budget, :] = 1
 
     zeros_index = torch.zeros_like(accumulated_attention_score, dtype=torch.bool)
 
@@ -45,8 +32,6 @@ def local_heavy_hitter_mask(attn_weights, heavy_budget):
         tmp_topk_index = accumulated_attention_score.topk(k=heavy_budget - 1, dim=-1).indices
 
         mask_bottom_index = zeros_index.scatter(-1, tmp_topk_index, True) #(head, keys)
-
-        #alpha[:,:,token_index,:] = torch.sum(tmp_attn[:,:,token_index - 1,:] * mask_bottom_index, dim=-1, keepdim=True)
 
         mask_bottom_index[:,:, token_index] = True 
 
